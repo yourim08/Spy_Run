6d4783be2853886983f3bcc8c0ccb15fdd6a6e9a
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2025-06-05 10:07:53 +0900

    docs : 정보 보안 처리
    DB 정보 보안화

--- SPY_Run/src/SPY_RUN/Test.java
+++ SPY_Run/src/SPY_RUN/Test.java
@@ -6,7 +6,7 @@ public class Test {
    public static void main(String args[]) {
       try {
          Class.forName("com.mysql.jdbc.Driver");
-         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spyrundb", "root","qwer1357!");
+         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spyrund", "root","#");
          System.out.println("DB 연결 완료!");
       } catch (ClassNotFoundException e) {
          System.out.println("JDBC 드라이버 로드 에러");
